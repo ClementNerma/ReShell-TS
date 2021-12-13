642ea8cfefa2e9f0336d1268ed21a3862e8e9536
Clément Nerma <clement.nerma@gmail.com>
2021-12-13 01:02:22 +0100

    Add the 'unify' parser

--- src/lib/transform.ts
+++ src/lib/transform.ts
@@ -28,6 +28,10 @@ export function silence<T>(parser: Parser<T>): Parser<void> {
   return map(parser, (_) => {})
 }
 
+export function unify(parser: Parser<unknown>): Parser<string> {
+  return map(parser, (_, parsed) => parsed.matched)
+}
+
 export function toOneProp<P extends string, T>(parser: Parser<T>, prop: P): Parser<{ [prop in P]: Token<T> }> {
   return map(parser, (_, value) => ({ [prop]: value } as { [prop in P]: Token<T> }))
 }
