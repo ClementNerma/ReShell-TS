f9678192ae892181575255fc469b800036248d93
Clément Nerma <clement.nerma@gmail.com>
2021-12-13 01:02:35 +0100

    Make nested file inclusions work correctly in runtime engine

--- src/runtime/block.ts
+++ src/runtime/block.ts
@@ -1,16 +1,19 @@
-import { Block } from '../shared/ast'
+import { Block, Statement } from '../shared/ast'
+import { Token } from '../shared/parsed'
 import { Runner, success } from './base'
 import { runStatement } from './statement'
 
 export const runBlock: Runner<Block> = (block, ctx) => {
   ctx = { ...ctx, scopes: ctx.scopes.concat([{ generics: [], entities: new Map() }]) }
 
-  const flattened = block.map((stmt) => (stmt.parsed.type === 'fileInclusion' ? stmt.parsed.content : [stmt])).flat()
-
-  for (const stmt of flattened) {
+  for (const stmt of flattenBlock(block)) {
     const result = runStatement(stmt, ctx)
     if (result.ok !== true) return result
   }
 
   return success(void 0)
+}
+
+function flattenBlock(block: Block): Token<Statement>[] {
+  return block.map((stmt) => (stmt.parsed.type === 'fileInclusion' ? flattenBlock(stmt.parsed.content) : [stmt])).flat()
 }
