6e45ecd33dc81a6fdcaf32c9c11744b34de1fcd7
Clément Nerma <clement.nerma@gmail.com>
2021-12-13 01:02:29 +0100

    Provide context to mappers in map() and mapFull() transformers

--- src/parsers/lib/transform.ts
+++ src/parsers/lib/transform.ts
@@ -1,27 +1,27 @@
 import { Token } from '../../shared/parsed'
-import { Parser, withErr, WithErrData } from './base'
+import { Parser, ParsingContext, withErr, WithErrData } from './base'
 
 export function map<T, U>(
   parser: Parser<T>,
-  mapper: (value: T, parsed: Token<T>) => U,
+  mapper: (value: T, parsed: Token<T>, context: ParsingContext) => U,
   error?: WithErrData
 ): Parser<U> {
   return (start, input, context) => {
     const parsed = parser(start, input, context)
     return parsed.ok
-      ? { ...parsed, data: { ...parsed.data, parsed: mapper(parsed.data.parsed, parsed.data) } }
+      ? { ...parsed, data: { ...parsed.data, parsed: mapper(parsed.data.parsed, parsed.data, context) } }
       : withErr(parsed, error)
   }
 }
 
 export function mapFull<T, U>(
   parser: Parser<T>,
-  mapper: (value: T, parsed: Token<T>) => Token<U>,
+  mapper: (value: T, parsed: Token<T>, context: ParsingContext) => Token<U>,
   error?: WithErrData
 ): Parser<U> {
   return (start, input, context) => {
     const parsed = parser(start, input, context)
-    return parsed.ok ? { ok: true, data: mapper(parsed.data.parsed, parsed.data) } : withErr(parsed, error)
+    return parsed.ok ? { ok: true, data: mapper(parsed.data.parsed, parsed.data, context) } : withErr(parsed, error)
   }
 }
 
