f8b4590c4d45e0f16743343037975e6271a95363
Clément Nerma <clement.nerma@gmail.com>
2021-12-13 01:02:32 +0100

    Add the first builtin variables, `argv` :rocket:

--- src/typechecker/scope/native-lib.ts
+++ src/typechecker/scope/native-lib.ts
@@ -1,7 +1,28 @@
+import { CodeSection } from '../../shared/parsed'
 import { Scope } from '../base'
 
 export function nativeLibraryScope(): Scope {
   const nativeLib: Scope = new Map()
 
+  nativeLib.set('argv', {
+    type: 'var',
+    at: nativeLibAt,
+    mutable: false,
+    varType: { type: 'list', itemsType: { type: 'unknown' } }
+  })
+
   return nativeLib
+}
+
+const nativeLibAt: CodeSection = {
+  start: {
+    file: { type: 'internal', path: 'native library' },
+    col: 0,
+    line: 0
+  },
+  next: {
+    file: { type: 'internal', path: 'native library' },
+    col: 0,
+    line: 0
+  }
 }
