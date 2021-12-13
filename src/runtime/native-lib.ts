c3466f397b4e01ce693295b1d26765b2601c2c45
Clément Nerma <clement.nerma@gmail.com>
2021-12-13 01:02:36 +0100

    Simplify writing of native library function executors

--- src/runtime/native-lib.ts
+++ src/runtime/native-lib.ts
@@ -6,55 +6,64 @@ import { CodeSection } from '../shared/parsed'
 import { matchUnion } from '../shared/utils'
 import { err, ExecValue, RunnerContext, RunnerResult, success } from './base'
 
-function getArguments<A extends { [name: string]: ValueType['type'] }, H extends keyof A | false = false>(
+function withArguments<A extends { [name: string]: ValueType['type'] | { nullable: ValueType['type'] } }>(
   at: CodeSection,
   map: Map<string, ExecValue>,
   expecting: A,
-  hidden?: Array<H>
-): RunnerResult<{
-  [name in keyof A]: Extract<
-    ExecValue,
-    H extends false ? { type: A[name] } : H extends name ? { type: A[name] } | { type: 'null' } : { type: A[name] }
-  >
-}> {
+  callback: (data: {
+    [name in keyof A]: Extract<
+      ExecValue,
+      A[name] extends { nullable: ValueType['type'] }
+        ? { type: 'null' } | { type: A[name]['nullable'] }
+        : { type: A[name] }
+    >
+  }) => RunnerResult<ExecValue | null>
+): RunnerResult<ExecValue> {
   const out: object = {}
 
   for (const [name, type] of Object.entries(expecting)) {
     const value = map.get(name)
 
+    let expectedType: ValueType['type']
+
     if (value === undefined)
       return err(at, `internal error in native library executor: argument "${name}" was not found`)
 
-    if (value.type !== type && type !== 'unknown' && !(value.type === 'null' && hidden?.includes(name as H) === true)) {
+    if (typeof type === 'string') {
+      expectedType = type
+    } else {
+      if (value.type === 'null') {
+        continue
+      }
+
+      expectedType = type.nullable
+    }
+
+    if (value.type !== expectedType && expectedType !== 'unknown') {
       return err(
         at,
-        `internal error in native library executor: expected argument "${name}" to be of type "${type}", found ${value.type}`
+        `internal error in native library executor: expected argument "${name}" to be of type "${expectedType}", found ${value.type}`
       )
     }
+
+    map.delete(name)
 
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     out[name] = value
   }
 
+  if (map.size > 0) {
+    return err(
+      at,
+      'internal error in native library executor: unknown arguments provided: ' + [...map.keys()].join(', ')
+    )
+  }
+
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  return success(out)
+  return callback(out)
 }
-
-export const nativeLibraryVariables = buildWithNativeLibraryVarNames<(ctx: RunnerContext) => ExecValue>({
-  argv: (ctx) => ({
-    type: 'list',
-    items: ctx.argv.map((value) => ({ type: 'string', value })),
-  }),
-  PATH: () => ({
-    type: 'list',
-    items:
-      process.env['PATH'] !== undefined
-        ? process.env['PATH'].split(':').map((entry) => ({ type: 'string', value: entry }))
-        : [],
-  }),
-})
 
 export type NativeFn = (
   input: {
@@ -66,110 +75,73 @@ export type NativeFn = (
 ) => RunnerResult<ExecValue | null>
 
 export const nativeLibraryFunctions = buildWithNativeLibraryFunctionNames<NativeFn>({
-  ok: ({ at }, map) => {
-    const args = getArguments(at, map, { value: 'unknown' })
-    if (args.ok !== true) return args
+  ok: ({ at }, map) =>
+    withArguments(at, map, { value: 'unknown' }, ({ value }) => success({ type: 'failable', success: true, value })),
 
-    const { value } = args.data
-    return success({ type: 'failable', success: true, value })
-  },
+  err: ({ at }, map) =>
+    withArguments(at, map, { error: 'unknown' }, ({ error }) =>
+      success({ type: 'failable', success: false, value: error })
+    ),
 
-  err: ({ at }, map) => {
-    const args = getArguments(at, map, { error: 'unknown' })
-    if (args.ok !== true) return args
+  typed: ({ at }, map) => withArguments(at, map, { value: 'unknown' }, ({ value }) => success(value)),
 
-    const { error } = args.data
-    return success({ type: 'failable', success: false, value: error })
-  },
+  toFixed: ({ at }, map) =>
+    withArguments(at, map, { number: 'number', precision: 'number' }, ({ number, precision }) =>
+      success({
+        type: 'string',
+        value: number.value.toFixed(precision.value),
+      })
+    ),
 
-  typed: ({ at }, map) => {
-    const args = getArguments(at, map, { value: 'unknown' })
-    if (args.ok !== true) return args
+  listAt: ({ at }, map) =>
+    withArguments(at, map, { list: 'list', index: 'number' }, ({ list, index }) => {
+      const item = list.items.at(index.value)
+      return success(item ?? { type: 'null' })
+    }),
 
-    const { value } = args.data
-    return { ok: null, breaking: 'return', value }
-  },
+  repeat: ({ at }, map) =>
+    withArguments(at, map, { str: 'string', repeat: 'number' }, ({ str, repeat }) =>
+      success({ type: 'string', value: str.value.repeat(repeat.value) })
+    ),
 
-  toFixed: ({ at }, map) => {
-    const args = getArguments(at, map, { number: 'number', precision: 'number' })
-    if (args.ok !== true) return args
+  echo: ({ at, pipeTo }, map) =>
+    withArguments(at, map, { message: 'string', n: 'bool' }, ({ message, n }) => {
+      pipeTo.stdout.write(n.value ? message.value : message.value + '\n')
+      return success(null)
+    }),
 
-    const { number, precision } = args.data
-    return success({ type: 'string', value: number.value.toFixed(precision.value) })
-  },
+  dump: ({ at, ctx, pipeTo }, map) =>
+    withArguments(at, map, { value: 'unknown', pretty: 'bool' }, ({ value, pretty }) => {
+      pipeTo.stdout.write(valueToStr(value, pretty.value, true, ctx) + '\n')
+      return success(null)
+    }),
 
-  listAt: ({ at }, map) => {
-    const args = getArguments(at, map, { list: 'list', index: 'number' })
-    if (args.ok !== true) return args
+  toStr: ({ at, ctx }, map) =>
+    withArguments(at, map, { value: 'unknown', pretty: 'bool' }, ({ value, pretty }) =>
+      success({
+        type: 'string',
+        value: valueToStr(value, pretty.value, false, ctx),
+      })
+    ),
 
-    const { list, index } = args.data
+  trace: ({ pipeTo, at }, map) =>
+    withArguments(at, map, {}, () => {
+      const file: string = matchUnion(at.start.file, 'type', {
+        entrypoint: ({ path }) => path,
+        file: ({ path }) => path,
+        internal: ({ path }) => `<internal:${path}>`,
+      })
 
-    const item = list.items.at(index.value)
-    return success(item ?? { type: 'null' })
-  },
+      pipeTo.stdout.write(`[Trace] ${file}:${at.start.line + 1}:${at.start.col + 1}\n`)
 
-  repeat: ({ at }, map) => {
-    const args = getArguments(at, map, { str: 'string', repeat: 'number' })
-    if (args.ok !== true) return args
+      return success(null)
+    }),
 
-    const { str, repeat } = args.data
+  ls: ({ at, ctx }, map) =>
+    withArguments(at, map, { path: { nullable: 'path' } }, ({ path }) => {
+      const dir = path.type === 'null' ? process.cwd() : path.segments.join(ctx.platformPathSeparator)
 
-    return success({ type: 'string', value: str.value.repeat(repeat.value) })
-  },
-
-  echo: ({ at, pipeTo }, map) => {
-    const args = getArguments(at, map, { message: 'string', n: 'bool' })
-    if (args.ok !== true) return args
-
-    const { message, n } = args.data
-    pipeTo.stdout.write(n.value ? message.value : message.value + '\n')
-
-    return success(null)
-  },
-
-  dump: ({ at, ctx, pipeTo }, map) => {
-    const args = getArguments(at, map, { value: 'unknown', pretty: 'bool' })
-    if (args.ok !== true) return args
-
-    const { value, pretty } = args.data
-    pipeTo.stdout.write(valueToStr(value, pretty.value, true, ctx) + '\n')
-
-    return success(null)
-  },
-
-  toStr: ({ at, ctx }, map) => {
-    const args = getArguments(at, map, { value: 'unknown', pretty: 'bool' })
-    if (args.ok !== true) return args
-
-    const { value, pretty } = args.data
-
-    return success({ type: 'string', value: valueToStr(value, pretty.value, false, ctx) })
-  },
-
-  trace: ({ pipeTo, at }) => {
-    const file: string = matchUnion(at.start.file, 'type', {
-      entrypoint: ({ path }) => path,
-      file: ({ path }) => path,
-      internal: ({ path }) => `<internal:${path}>`,
-    })
-
-    pipeTo.stdout.write(`[Trace] ${file}:${at.start.line + 1}:${at.start.col + 1}\n`)
-
-    return { ok: null, breaking: 'return', value: null }
-  },
-
-  ls: ({ at, ctx }, map) => {
-    const args = getArguments(at, map, { path: 'path' }, ['path'])
-    if (args.ok !== true) return args
-
-    const { path } = args.data
-
-    const dir = path.type === 'null' ? process.cwd() : path.segments.join(ctx.platformPathSeparator)
-
-    return {
-      ok: null,
-      breaking: 'return',
-      value: {
+      return success({
         type: 'list',
         items: readdirSync(dir).map((name): ExecValue => {
           const item = lstatSync(join(dir, name))
@@ -198,9 +170,8 @@ export const nativeLibraryFunctions = buildWithNativeLibraryFunctionNames<Native
             ]),
           }
         }),
-      },
-    }
-  },
+      })
+    }),
 })
 
 const valueToStr = (value: ExecValue, pretty: boolean, dumping: boolean, ctx: RunnerContext): string =>
@@ -256,3 +227,17 @@ const valueToStr = (value: ExecValue, pretty: boolean, dumping: boolean, ctx: Ru
     failable: ({ success, value }) => `${success ? 'ok' : 'err'}(${valueToStr(value, pretty, dumping, ctx)})`,
     rest: () => `<rest>`,
   })
+
+export const nativeLibraryVariables = buildWithNativeLibraryVarNames<(ctx: RunnerContext) => ExecValue>({
+  argv: (ctx) => ({
+    type: 'list',
+    items: ctx.argv.map((value) => ({ type: 'string', value })),
+  }),
+  PATH: () => ({
+    type: 'list',
+    items:
+      process.env['PATH'] !== undefined
+        ? process.env['PATH'].split(':').map((entry) => ({ type: 'string', value: entry }))
+        : [],
+  }),
+})
