547bd59ab3dbdc43f622074e1a9276039dae7796
Clément Nerma <clement.nerma@gmail.com>
2021-12-13 01:02:24 +0100

    Rename typechecker internal types

--- src/typechecker/program.ts
+++ src/typechecker/program.ts
@@ -1,15 +1,15 @@
 import { Program } from '../parsers/data'
-import { Executor, success } from './types'
+import { success, Typechecker } from './types'
 
 // Global architecture: each file corresponds to a section of the AST and has its own:
-// * Executor
+// * Typechecker
 // * Context (if any)
 // * Output type
 // * Error type
 
 export type ProgramError = void
 
-export const programChecker: Executor<Program, void, void, ProgramError> = (input, context) => {
+export const programChecker: Typechecker<Program, void, void, ProgramError> = (input, context) => {
   console.log('TODO!')
   return success(void 0)
 }
--- src/typechecker/types.ts
+++ src/typechecker/types.ts
@@ -1,8 +1,8 @@
 import { ParserLoc, Token } from '../lib/base'
 
-export type Executor<T, C, O, E> = (input: Token<T>, context: C) => ExecutorResult<O, E>
+export type Typechecker<T, C, O, E> = (input: Token<T>, context: C) => TypecheckerResult<O, E>
 
-export type ExecutorResult<O, E> = { ok: true; data: O } | { ok: false; err: E; loc: ParserLoc }
+export type TypecheckerResult<O, E> = { ok: true; data: O } | { ok: false; err: E; loc: ParserLoc }
 
-export const success = <O, E>(data: O): ExecutorResult<O, E> => ({ ok: true, data })
-export const err = <O, E>(err: E, loc: ParserLoc): ExecutorResult<O, E> => ({ ok: false, err, loc })
+export const success = <O, E>(data: O): TypecheckerResult<O, E> => ({ ok: true, data })
+export const err = <O, E>(err: E, loc: ParserLoc): TypecheckerResult<O, E> => ({ ok: false, err, loc })
