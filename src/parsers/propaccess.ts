da6c7a14c41866cb663225ae1791490d6818a49a
Clément Nerma <clement.nerma@gmail.com>
2021-12-13 01:02:28 +0100

    Simplify parsing of property access

--- src/parsers/propaccess.ts
+++ src/parsers/propaccess.ts
@@ -2,10 +2,9 @@ import { NonNullablePropertyAccess, PropertyAccess } from '../shared/parsed'
 import { expr } from './expr'
 import { Parser } from './lib/base'
 import { combine } from './lib/combinations'
-import { maybe } from './lib/conditions'
-import { contextualFailure } from './lib/errors'
+import { failure } from './lib/errors'
 import { exact } from './lib/matchers'
-import { mappedCases } from './lib/switches'
+import { mappedCases, or } from './lib/switches'
 import { map } from './lib/transform'
 import { withLatelyDeclared } from './lib/utils'
 import { identifier } from './tokens'
@@ -25,14 +24,16 @@ export const nonNullablePropertyAccess: Parser<NonNullablePropertyAccess> = mapp
   }
 )
 
-export const propertyAccess: Parser<PropertyAccess> = map(
-  combine(
-    maybe(exact('?')),
-    contextualFailure(
-      nonNullablePropertyAccess,
-      (ctx) => !ctx.combinationData!.soFar.previousInfos!.phantomSuccess,
-      'Expected a property index, key or member name after optional chaining operator (?.)'
-    )
+export const propertyAccess: Parser<PropertyAccess> = or<PropertyAccess>([
+  map(nonNullablePropertyAccess, (access) => ({ nullable: false, access })),
+  map(
+    combine(
+      exact('?'),
+      failure(
+        nonNullablePropertyAccess,
+        'Expected a property index, key or member name after optional chaining operator (?.)'
+      )
+    ),
+    ([_, { parsed: access }]) => ({ nullable: true, access })
   ),
-  ([{ parsed: nullable }, { parsed: access }]) => ({ nullable: nullable !== null, access })
-)
+])
